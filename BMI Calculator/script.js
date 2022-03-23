4ef7e691feea40c6d9c3dd9776329b4b83d9e2c5
C-112 Fajer Almarri <70756233+C-112FajerALmarri@users.noreply.github.com>
2022-03-23 09:52:19 +0300

    done

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,1 +1,31 @@
 // هنا سوف تنشئ دالة calculate 
+
+function calculate(){
+
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+
+
+    let BMI = weight/(height*height);
+    let status ="";
+    if(BMI <18.5)
+    {
+        status ="يعاني من نقص في الوزن"
+}
+else if (BMI <25){
+
+    status = "وزنك صحي"
+}
+else if (BMI >=25&& BMI <30 ){"تعاني من زيادة في الوزن"
+
+}
+
+
+
+
+
+
+document.getElementById("result").innerHTML= BMI ;
+document.getElementsByClassName("comment")[0].innerHTML=status;
+}
+
